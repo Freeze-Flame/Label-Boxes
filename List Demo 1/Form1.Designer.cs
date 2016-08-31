71ab6cfd22ff22cb1fa935180cb4e3ba5bc47321
ssalyer1 <ssalyer1@dtcc.edu>
2016-08-31 09:35:02 -0400

    Dont worry. Im dead

--- List Demo 1/Form1.Designer.cs
+++ List Demo 1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace List_Demo_1
 {
-    partial class Form1
+    partial class TimeZones
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbxCities = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTimeZone = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxCities
+            // 
+            this.lbxCities.FormattingEnabled = true;
+            this.lbxCities.Items.AddRange(new object[] {
+            "Honolulu",
+            "San Francisco",
+            "Denver",
+            "Minneapolis",
+            "New York",
+            "Chicago",
+            "Misueahvasoa",
+            "Los Angeles"});
+            this.lbxCities.Location = new System.Drawing.Point(78, 64);
+            this.lbxCities.Name = "lbxCities";
+            this.lbxCities.Size = new System.Drawing.Size(120, 69);
+            this.lbxCities.TabIndex = 0;
+            this.lbxCities.SelectedIndexChanged += new System.EventHandler(this.lbxCities_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time Zone";
+            // 
+            // lblTimeZone
+            // 
+            this.lblTimeZone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTimeZone.Location = new System.Drawing.Point(78, 169);
+            this.lblTimeZone.Name = "lblTimeZone";
+            this.lblTimeZone.Size = new System.Drawing.Size(126, 27);
+            this.lblTimeZone.TabIndex = 1;
+            this.lblTimeZone.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pick a city, and click it";
+            // 
+            // TimeZones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTimeZone);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxCities);
+            this.Name = "TimeZones";
+            this.Text = "Time Zones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbxCities;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTimeZone;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- List Demo 1/Form1.cs
+++ List Demo 1/Form1.cs
@@ -10,11 +10,51 @@ using System.Windows.Forms;
 
 namespace List_Demo_1
 {
-    public partial class Form1 : Form
+    public partial class TimeZones : Form
     {
-        public Form1()
+        public TimeZones()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbxCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string city;
+
+            // Get the city that was selected
+            city = lbxCities.SelectedItem.ToString();
+
+            // Switch based on the city selected
+            switch(city)
+            {
+                case "Honolulu":
+                    lblTimeZone.Text = "Hawdi-Aleutian";
+                    break;
+                case "San Francisco":
+                case "Los Angeles":
+                    lblTimeZone.Text = "Pacific";
+                    break;
+                case "Denver":
+                case "Misueahvasoa":
+                    lblTimeZone.Text = "Mountain";
+                    break;
+                case "Minneapolis":
+                case "Chicago":
+                    lblTimeZone.Text = "Central";
+                    break;
+                case "New York":
+                    lblTimeZone.Text = "Eastern";
+                    break;
+                default:
+                    MessageBox.Show("City Not Found", "City Lost",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
         }
     }
 }
